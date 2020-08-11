4c80246dc74146ff466252ceea148251ad72b8b2
Sebastian Gracia <sebastiangracia123@gmail.com>
2020-08-11 18:28:32 -0500

    Objects, and array problem set

---
+++ HTML/JavaScript/Objects/script.js
@@ -0,0 +1,11 @@
+let human = {
+    name: 'Caro',
+    age: 26,
+    city: 'LA'
+};
+
+let person = {};
+person.age = 1;
+person.city = 'LA';
+person.name = 'New born';
+console.log(person.name);
--- HTML/JavaScript/Todo list/script.js
+++ HTML/JavaScript/Todo list/script.js
@@ -4,12 +4,24 @@ window.setTimeout(function (){
     while (option !== 'quit') {
         switch (option) { // You can do this by using if statements...
             case 'list':
+                console.log('********');
                 console.log(todoArray);
+                console.log('********');
                 break;
             case 'new':
+                console.log('********');
                 let todo = prompt('Enter a new todo...');
+                console.log('********');
                 todoArray.push(todo);
                 todo = '';
+                break;
+            case 'delete':
+                console.log('********');
+                todoArray.forEach(printTodo);
+                let deletion = prompt('What would you like to delete?');
+                console.log('********');
+                todoArray.splice(parseInt(deletion),1);
+                todoArray.forEach(printTodo);
                 break;
             case 'quit':
                 option = 'quit';
@@ -21,3 +33,7 @@ window.setTimeout(function (){
         option = prompt('What would you like to do?');
     }
 }, 5000);
+
+function printTodo (arr, index) {
+    console.log(index + ': ' + arr);
+}
